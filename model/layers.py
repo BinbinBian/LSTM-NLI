added full network code... gradient compilation bug to fix

model/layers.py
@@ -20,15 +20,20 @@ class HiddenLayer(object):
         """
         # Dictionary of model parameters.
         self.params = {}
-
+        self.layerName = layerName
         self.inputDim = dimInput
         self.dimHidden = dimHiddenState
 
-        self.numLabels = numCategories # Represents number of categories used for classification
+        # Represents number of categories used for classification
+        self.numLabels = numCategories
 
-        self.outputs = None
+        # Initialization values for hidden layer compute unit parameters
+        self.hiddenInit = None
+        self.candidateValInit = None
 
         # TODO: what to use for initializing parameters (random?)
+
+        # TODO: include parameters for projecting from input word vector dimensions to dimInput
 
         # Parameters for forget gate
         self.b_f = theano.shared(np.random.randn(1, dimHiddenState), name="biasF_"+layerName, broadcastable=(True, False))
@@ -104,7 +109,28 @@ class HiddenLayer(object):
         """
         self.params.update(newParams)
 
-    
+
+    def printParams(self):
+        """
+        Prints current parameters of model for debugging purposes.
+        """
+        print "Current parameter values for %s" %(self.layerName)
+        print "-" * 50
+        for pName, pValue in self.params.iteritems():
+            print pName, " : ", pValue.eval()
+
+        print "-" * 50
+
+    def setInitialLayerParams(self, hiddenInit, candidateValInit):
+        """
+        Sets the initialization hidden value and candidate value parameters
+        :param hiddenValInit:
+        :param candidateValInit:
+        """
+        self.hiddenInit = hiddenInit
+        self.candidateValInit = candidateValInit
+
+
     def _step(self, input, prevHiddenState, prevCellState):
         """
         Function used for executing computation of one
@@ -143,7 +169,7 @@ class HiddenLayer(object):
 
         return hiddenState, candidateVals
 
-    def forwardRun(self, inputMat, timeSteps, numSamples):
+    def forwardRun(self, inputMat, timeSteps):
         """
         Executes forward computation for designated number of time steps.
         Returns output vectors for all timesteps.
@@ -153,8 +179,13 @@ class HiddenLayer(object):
         """
         #print "Input mat shape: ", inputMat.shape.eval()
 
-        hiddenInit = T.unbroadcast(T.alloc(np.cast[theano.config.floatX](1.), inputMat.shape[1], self.dimHidden),0)
-        candidateValsInit = T.unbroadcast(T.alloc(np.cast[theano.config.floatX](1.), inputMat.shape[1], self.dimHidden), 0)
+        # Outputs of premise layer passed as input to hypothesis layer
+        if self.hiddenInit is None and self.candidateValInit is None:
+            hiddenInit = T.unbroadcast(T.alloc(np.cast[theano.config.floatX](1.), inputMat.shape[1], self.dimHidden),0)
+            candidateValsInit = T.unbroadcast(T.alloc(np.cast[theano.config.floatX](1.), inputMat.shape[1], self.dimHidden), 0)
+        else:
+            hiddenInit = self.hiddenInit
+            candidateValsInit = self.candidateValInit
 
         #print hiddenInit.eval()
         #print candidateValsInit.eval()
@@ -211,18 +242,18 @@ class HiddenLayer(object):
         return T.mean(T.eq(T.argmax(yPred, axis=-1), T.argmax(yTarget, axis=-1)))
 
 
-    def costFunc(self, x, yTarget, numTimesteps):
+    def costFunc(self, x, yTarget, numTimesteps=1):
         """
         Compute end-to-end cost function for a collection of input data.
         :return: Symbolic expression for cost function as well as theano function
                  for computing cost expression.
         """
-        _ = self.forwardRun(x, numTimesteps, 100) # Last parameter is num Samples -- may want to remove that
-        catOutput = self._projectToCategories()
-        softmaxOut = self._getPredictions(catOutput)
-        cost = self._computeCrossEntropyCost(softmaxOut, yTarget)
-
-        return cost, theano.function([x, yTarget], cost, name='LSTM_cost_function')
+        _ = self.forwardRun(x, numTimesteps) # Last parameter is num Samples -- may want to remove that
+        catOutput = self.projectToCategories()
+        softmaxOut = self.getPredictions(catOutput)
+        cost = self.computeCrossEntropyCost(softmaxOut, yTarget)
+        theano.printing.pydotprint(cost, outfile="costGraph")
+        return cost #, theano.function([x, yTarget], cost, name='LSTM_cost_function')
 
 
     def computeGrads(self, x, yTarget, cost):
@@ -231,9 +262,11 @@ class HiddenLayer(object):
         :param costFunc:
         :return:
         """
+        theano.pp(cost)
         grads = T.grad(cost, wrt=self.params.values())
-        costGrad = theano.function([x, yTarget], grads, name='costGradients')
-        return grads, costGrad
+        #theano.pp(grads)
+        #costGrad = theano.function([x, yTarget], grads, name='costGradients')
+        return grads #, costGrad
 
 
     def sgd(self, grads, learnRate):

model/network.py
@@ -14,7 +14,7 @@ class Network(object):
     """
     Represents entire network.
     """
-    def __init__(self, numTimesteps=1, dimHidden=5, dataPath=None, optimizer="sgd"):
+    def __init__(self, numTimesteps=1, dimHidden=2, dataPath=None, optimizer="sgd"):
         """
         :param numTimesteps: Number of timesteps to unroll network for.
         :param dataPath: Path to file with precomputed word embeddings
@@ -25,39 +25,68 @@ class Network(object):
         """
         self.numTimesteps = numTimesteps
         self.embeddingTable = EmbeddingTable(dataPath)
-        self.dimInput = self.embeddingTable.dimEmbeddings
+        #self.dimInput = self.embeddingTable.dimEmbeddings
+        self.dimInput = 2
         self.dimHidden = dimHidden
-        self.optimizer = optimizer
+        self.inputMat = None # To store matrix of data input
 
-    def _buildModel(self, ):
+    def buildModel(self):
         """
         Handles building of model, including initializing necessary parameters, defining
         loss functions, etc.
         """
-        self.hiddenLayer = HiddenLayer(self.dimHidden, self.dimInput)
-        #self.hiddenLayer.forwardRun(self.numTimesteps)
+        self.hiddenLayerPremise = HiddenLayer(self.dimHidden, self.dimInput, "premiseLayer")
+        self.hiddenLayerHypothesis = HiddenLayer(self.dimHidden, self.dimInput, "hypothesisLayer")
 
-        prediction = T.nnet.softmax(T.dot(self.hiddenLayer.outputs, # May want to consider making outputs (numSamples, dimOutput) dimension
-                                        self.hiddenLayer.weightsOutputTransform)
-                                        + self.hiddenLayer.biasOutputTransform)
-        predictionProbs = theano.function([input], prediction, name='predictionProbs')
-
-        loss = -1 * T.log(prediction[T.arange(10), np.arange(10)]).mean() # TODO: need to fix this
+        self.hiddenLayerHypothesis.updateParams(self.hiddenLayerPremise.params)
 
 
-        raise NotImplementedError
+    def trainFunc(self):
+        inputPremise = T.dtensor3("inputPremise")
+        inputHypothesis = T.dtensor3("inputHypothesis")
+        yTarget = T.dmatrix("yTarget")
 
-    def train(self, numEpochs=5, batchSize=10):
+        # self.hiddenLayerHypothesis.printParams()
+        # del self.hiddenLayerHypothesis.params["weightsXo_hypothesisLayer"]
+        # del self.hiddenLayerHypothesis.params["weightsCat_premiseLayer"]
+        # del self.hiddenLayerHypothesis.params["biasO_hypothesisLayer"]
+
+        # self.hiddenLayerHypothesis.printParams()
+
+        self.hiddenLayerPremise.forwardRun(inputPremise, 1)
+        premiseOutputHidden = self.hiddenLayerPremise.finalHiddenVal
+        premiseOutputCandidate = self.hiddenLayerPremise.finalCandidateVal
+
+        #print "premise Output Hidden: ", premiseOutputHidden
+
+        self.hiddenLayerHypothesis.setInitialLayerParams(premiseOutputHidden, premiseOutputCandidate)
+        self.hiddenLayerHypothesis.forwardRun(inputHypothesis, 1)
+        hypothesisOutput = self.hiddenLayerHypothesis.finalHiddenVal
+        cost = self.hiddenLayerHypothesis.costFunc(inputHypothesis, yTarget)
+        grads = self.hiddenLayerHypothesis.computeGrads(inputHypothesis, yTarget, cost)
+        trainF = theano.function([inputPremise, inputHypothesis, yTarget], grads)
+
+
+        premiseSent = np.random.randn(1,1,2)
+        hypothesisSent = np.random.randn(1,1,2)
+        xNP = np.array([[[0.3, 0.04]]], dtype = np.float64)
+        yTargetVal = np.array([[0., 1., 0.]], dtype=np.float64)
+
+        print "Train out: ", trainF(premiseSent, hypothesisSent, yTargetVal)
+
+
+    def train(self, numEpochs=5, batchSize=1):
         """
         Takes care of training model, including propagation of errors and updating of
         parameters.
         """
+        #for epoch in xrange(numEpochs):
 
-        # Will have entire dataset here, represented as (numSamples, numTimesteps) where each entry
-        # is index of appropriate lookup in embedding table
+        premiseSent = T.as_tensor_variable(np.random.randn(1,1,2))
+        hypothesisSent= T.as_tensor_variable(np.random.randn(1,1,2))
 
-        # Will make call to _buildModel here
-        raise NotImplementedError
+
+
 
 
 

test/functionality.py
@@ -8,6 +8,7 @@ import theano.tensor as T
 
 from model.embeddings import EmbeddingTable
 from model.layers import HiddenLayer
+from model.network import Network
 from util.utils import convertLabelsToMat
 
 dataPath = "/Users/mihaileric/Documents/Research/LSTM-NLI/data/"
@@ -147,6 +148,7 @@ def testGradComputation():
     grads, gradFunc = hLayer.computeGrads(x, yTarget, cost)
     print "Grads: ", gradFunc(xNP, yTargetNP)
 
+
 def testSGD():
     hLayer = HiddenLayer(dimInput=2, dimHiddenState=2, layerName="testHidden")
     x = T.dtensor3("testX")
@@ -172,6 +174,22 @@ def testSGD():
         print name, ": ", param.eval()
 
 
+def testNetworkSetup():
+    network = Network()
+    network.buildModel()
+    network.hiddenLayerHypothesis.params["biasO_premiseLayer"] += T.as_tensor_variable(np.array([1, 2]))
+    network.hiddenLayerPremise.printParams()
+    network.hiddenLayerHypothesis.printParams()
+
+
+def testParamsBackPropUpdate():
+    """
+    Test to ensure that the parameters of premise are updated after backprop.
+    """
+    network = Network()
+    network.buildModel()
+    network.trainFunc()
+
 if __name__ == "__main__":
   # testLabelsMat()
   # testEmbeddings()
@@ -185,4 +203,6 @@ if __name__ == "__main__":
   #testCrossEntropyLoss()
   #testCostFuncPipeline()
   #testGradComputation()
-    testSGD()
+   # testSGD()
+   #testNetworkSetup()
+   testParamsBackPropUpdate()