860752aaab541a0e800e71d23aea4a2de880f2d1
Mihail Eric <mihail911@gmail.com>
2015-11-22 01:36:34 -0800

    wrote some of run script so that it will build network and train

--- test/run.py
+++ test/run.py
@@ -1,17 +1,25 @@
 """ Handles building, training, and testing the model.
 """
-
+import argparse
 import sys
 import os
 
-from keras.models import Sequential
+sys.path.append("/Users/mihaileric/Documents/Research/LSTM-NLI/")
 
-def buildModel():
-    pass
-
-def testModel():
-    pass
+from model.network import Network
 
 if __name__ == "__main__":
-    buildModel()
-    testModel()
+    parser = argparse.ArgumentParser(description="argument parser for neural model")
+
+    parser.add_argument("--batchSize", type=int,
+                        help="batch size for training")
+    parser.add_argument("--hiddenDim", type=int,
+                        help="dimension of hidden layer")
+    parser.add_argument("--numEpochs", type=int, default=4,
+                        help="number of epochs to use for training")
+    # TODO: Parse additional arguments as necessary
+
+    args = parser.parse_args()
+    network = Network() # Will pass arguments parsed here
+    network.buildModel()
+    network.train(args.numEpochs, args.batchSize)
